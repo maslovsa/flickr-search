443399770c30520aa4ff1264ddea3db5c4f6fc79
Maslov Sergey <sergey.maslov@rokolabs.com>
2015-09-11 16:58:33 +0300

    Initial Commit

---
+++ Flicker Search/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flicker Search
+//
+//  Created by Maslov Sergey on 11.09.15.
+//  Copyright © 2015 ROKOLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flicker Search/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flicker Search
+//
+//  Created by Maslov Sergey on 11.09.15.
+//  Copyright © 2015 ROKOLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
