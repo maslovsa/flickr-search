5662762046997488352a416bfaea1e3fb89631a6
Maslov Sergey <sergey.maslov@rokolabs.com>
2015-09-11 18:27:27 +0300

    init

--- Flicker Search/ViewController.m
+++ Flicker Search/ViewController.m
@@ -7,9 +7,19 @@
 //
 
 #import "ViewController.h"
+#import "Flickr.h"
+#import "FlickrPhoto.h"
 
-@interface ViewController ()
 
+@interface ViewController () <UITextFieldDelegate>
+@property(nonatomic, weak) IBOutlet UIToolbar *toolbar;
+@property(nonatomic, weak) IBOutlet UIBarButtonItem *shareButton;
+@property(nonatomic, weak) IBOutlet UITextField *textField;
+- (IBAction)shareButtonTapped:(id)sender;
+
+@property(nonatomic, strong) NSMutableDictionary *searchResults;
+@property(nonatomic, strong) NSMutableArray *searches;
+@property(nonatomic, strong) Flickr *flickr;
 @end
 
 @implementation ViewController
@@ -17,6 +27,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_cork.png"]];
+    
+    UIImage *navBarImage = [[UIImage imageNamed:@"navbar.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(27, 27, 27, 27)];
+    [self.toolbar setBackgroundImage:navBarImage forToolbarPosition:UIToolbarPositionAny
+                          barMetrics:UIBarMetricsDefault];
+    
+    UIImage *shareButtonImage = [[UIImage imageNamed:@"button.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(8, 8, 8, 8)];
+    [self.shareButton setBackgroundImage:shareButtonImage forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    
+    UIImage *textFieldImage = [[UIImage imageNamed:@"search_field.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(10, 10, 10, 10)];
+    [self.textField setBackground:textFieldImage];
+    
+    self.searches = [@[] mutableCopy];
+    self.searchResults = [@{} mutableCopy];
+    self.flickr = [Flickr new];
+                   
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +50,29 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(IBAction)shareButtonTapped:(id)sender {
+    // TODO
+}
+
+
+#pragma mark - UITextFieldDelegate methods
+- (BOOL) textFieldShouldReturn:(UITextField *)textField {
+    // 1
+    [self.flickr searchFlickrForTerm:textField.text completionBlock:^(NSString *searchTerm, NSArray *results, NSError *error) {
+        if(results && [results count] > 0) {
+            // 2
+            if(![self.searches containsObject:searchTerm]) {
+                NSLog(@"Found %d photos matching %@", [results count],searchTerm);
+                [self.searches insertObject:searchTerm atIndex:0];
+                self.searchResults[searchTerm] = results; }
+            // 3
+            dispatch_async(dispatch_get_main_queue(), ^{
+                // Placeholder: reload collectionview data
+            });
+        } else { // 1
+            NSLog(@"Error searching Flickr: %@", error.localizedDescription);
+        } }];
+    [textField resignFirstResponder];
+    return YES; 
+}
 @end
